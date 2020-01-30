bc8318f7f54367fe5f88992c68709e69955fe810
finn <isaaclindland@protonmail.com>
2020-01-30 15:48:44 -0700

    merging

---
+++ cmake-build-debug/CMakeFiles/tic_tac_toe_mcts.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tic_tac_toe_mcts.dir/main.cpp.o"
+  "tic_tac_toe_mcts"
+  "tic_tac_toe_mcts.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tic_tac_toe_mcts.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
